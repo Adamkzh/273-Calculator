6dd4c5a738f5d17991472ef99d84772875c07796
Adam Ke <mustkzh@gmail.com>
2017-10-05 23:28:35 -0700

    FB

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './lib/Calculator';
+import './index.css';
+
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('demo')
+);
